0197100c43e3e3bf1a916d95454a27d201bf104b
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2023-09-13 13:16:49 -0300

    feat(db)
    implementado uma pasta serviçes como arquivo de mock do banco de dados e as funções de leitura e salvamento do arquivo JSON

---
+++ app/backend/src/services/data.ts
@@ -0,0 +1,27 @@
+import fs from 'fs/promises'
+import path from 'path'
+import { Product } from '../models/Product'
+
+// Utilizei o método "loadDataFromFile e  saveDataToFile" para carregar e salvar os dados do arquivo JSON
+// pois é a maneira mais simples pra mim e ja tinha feito algo parecido em um projeto da trybe
+
+const filePath = path.resolve(__dirname, '../services/products.json')
+
+export async function loadDataFromFile() {
+  try {
+    const jsonData = await fs.readFile(filePath, 'utf-8')
+    const data = JSON.parse(jsonData)
+    return data
+  } catch (error) {
+    console.error('Erro ao carregar dados do arquivo:', error)
+  }
+}
+
+export async function saveDataToFile(data: Product) {
+  try {
+    const jsonData = JSON.stringify(data, null, 2)
+    await fs.writeFile(filePath, jsonData, 'utf-8')
+  } catch (error) {
+    throw error
+  }
+}
