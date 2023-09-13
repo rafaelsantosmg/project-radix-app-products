31bb32ce012e8ed8e9fb6449883e2206f3d7ec40
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2023-09-13 10:31:09 -0300

    feat(backend)
    iciando configuração do backend em graphql

--- app/backend/src/routers/userRouter.js
+++
@@ -1,9 +0,0 @@
-const rescue = require('express-rescue');
-const router = require('express').Router();
-const userController = require('../controllers/userControllers');
-const validateRequest = require('../middlewares/validateRequest');
-const { validateUser } = require('../schemas/joiSchemas');
-
-router.post('/', validateRequest(validateUser), rescue(userController.create));
-
-module.exports = router;
