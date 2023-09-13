cf251dce7995713cb34c76ef717a1352d051f6c3
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2023-09-13 10:30:16 -0300

    feat(frontend)
    iciando configuração do front end em nextJS

--- app/frontend/src/services/localStorage.js
+++
@@ -1,4 +0,0 @@
-export const saveUser = (user, token) => localStorage
-  .setItem('ebytr', JSON.stringify({ user, token }));
-
-export const getUser = () => JSON.parse(localStorage.getItem('ebytr'));
