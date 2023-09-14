ea4cb1f16d584ea0da18c3ca67ccd50992a9a359
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2023-09-14 12:22:26 -0300

    Atualização de script de instalação

--- apps_install.sh
+++ apps_install.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 printf "\n> Instalando o front-end\n"
-frontFolder="./app/frontend"
-cacheFolderFront="/tmp/frontend-cache"
-rm -rf $cacheFolderFront
-yarn_config_loglevel=silent yarn --prefix ${frontFolder} --cache $cacheFolderFront
+cd ./app/frontend
+yarn install 
 
 printf "\n> Instalando o back-end\n"
-backFolder="./app/backend"
-cacheFolderBack="/tmp/backend-cache"
-rm -rf $cacheFolderBack
-yarn_config_loglevel=silent yarn --prefix ${backFolder} --cache $cacheFolderBack
+cd ../backend
+yarn install  
