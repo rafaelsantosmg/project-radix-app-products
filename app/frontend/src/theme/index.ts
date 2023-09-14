6b067dd8ce6e3122103498e77516311b47190b24
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2023-09-14 10:47:04 -0300

    styles(themes)
    adcionado folha de temas a aplicação

---
+++ app/frontend/src/theme/index.ts
@@ -0,0 +1,11 @@
+const theme = {
+  gradientPurple: `linear-gradient(310.6deg, rgba(100, 0, 233, 0.94) 6.8%, rgba(166, 0, 188, 0.66) 57.8%)`,
+  gradientGray: `linear-gradient(109.6deg, rgb(0, 37, 84) 11.2%, rgba(0, 37, 84, 0.32) 100.2%)`,
+  purple: `#6400E9`,
+  white: `#FFF`,
+  black: `#000`,
+  gray: `#F5F5F5`,
+  red: `#FF0000`,
+}
+
+export default theme
