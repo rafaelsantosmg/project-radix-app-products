87d64eb9ff3175d854ad71397a311da66df5d419
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2023-09-14 10:47:57 -0300

    feet(hooks)
    adcionado hook persoinalizado para fazer a chamada get de produtos

---
+++ app/frontend/src/hooks/useGetProducts.tsx
@@ -0,0 +1,16 @@
+import { Product } from '@/interfaces/Products'
+import { useQuery } from '@apollo/client'
+import { GET_PRODUCTS } from '../../graphql/productQuery'
+
+function useGetProducts(): {
+  products: Product[]
+  loading: boolean
+} {
+  const { data, loading } = useQuery<{ products: Product[] }>(GET_PRODUCTS)
+
+  const products: Product[] = data?.products || []
+
+  return { products, loading }
+}
+
+export default useGetProducts
