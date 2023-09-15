2c32dd03637e68c6efd0b9f98435686cd1869cd8
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2023-09-15 15:33:13 -0300

    feat(queryProduct)
    adicionado a query de cração de produtos

--- app/frontend/src/graphql/productQuery.ts
+++ app/frontend/src/graphql/productQuery.ts
@@ -11,3 +11,26 @@ export const GET_PRODUCTS = gql`
     }
   }
 `
+export const CREATE_PRODUCT = gql`
+  mutation createProduct(
+    $id: String!
+    $name: String!
+    $description: String!
+    $category: String!
+    $price: Float!
+  ) {
+    createProduct(
+      id: $id
+      name: $name
+      description: $description
+      category: $category
+      price: $price
+    ) {
+      id
+      name
+      description
+      category
+      price
+    }
+  }
+`
