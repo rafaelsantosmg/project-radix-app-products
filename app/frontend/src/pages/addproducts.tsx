f3ef9f99e59d8615e8adab320c7003beb932664c
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2023-09-15 15:36:23 -0300

    feet(route)
    adcionado a rota addproducts

--- app/frontend/src/pages/addproducts.tsx
+++ app/frontend/src/pages/addproducts.tsx
@@ -1,7 +1,11 @@
+import Form from '../components/Form'
+import Header from '../components/Header'
+
 export default function AddProducts() {
   return (
-    <div>
-      <h1>Add Products</h1>
-    </div>
+    <>
+      <Header />
+      <Form />
+    </>
   )
 }
