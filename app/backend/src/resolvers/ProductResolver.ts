f60ff6c90158581c4c8647f30bca8f4d294d7f73
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2023-09-15 12:23:45 -0300

    feat(createProduct)
    implementado a função que permite adcionar um novo produto ao banco

--- app/backend/src/resolvers/ProductResolver.ts
+++ app/backend/src/resolvers/ProductResolver.ts
@@ -9,6 +9,30 @@ export class ProductResolver {
   @Query(() => [Product])
   async products() {
     this.data = await loadDataFromFile()
+    if (!this.data) {
+      throw new Error('No products found')
+    }
     return this.data
+  }
+
+  @Mutation(() => Product)
+  async createProduct(
+    @Arg('id') id: string,
+    @Arg('name') name: string,
+    @Arg('description') description: string,
+    @Arg('category') category: string,
+    @Arg('price') price: number
+  ) {
+    this.data = await loadDataFromFile()
+    const findProduct = this.data.find(
+      (product: Product) => product.id === id || product.name === name
+    )
+    if (findProduct) {
+      throw new Error('Product already exists')
+    }
+    const newProduct = { id, name, description, category, price }
+    this.data.push(newProduct)
+    await saveDataToFile(this.data)
+    return newProduct
   }
 }
