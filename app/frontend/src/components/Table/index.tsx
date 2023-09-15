bd8cd2922a3a14f8ea1de591b9aecbe19c1ba956
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2023-09-15 18:42:26 -0300

    styles(table)
    adcionado breakpoints da responsividade

--- app/frontend/src/components/Table/index.tsx
+++ app/frontend/src/components/Table/index.tsx
@@ -222,92 +222,96 @@ export default function SortTable() {
         width: '100%',
       }}
     >
-      <Paper sx={{ width: '90%', mb: 2 }}>
-        <TableContainer>
-          <Table
-            sx={{ minWidth: 750 }}
-            aria-labelledby="tableTitle"
-            size={dense ? 'small' : 'medium'}
-          >
-            <EnhancedTableHead
-              order={order}
-              orderBy={orderBy}
-              onRequestSort={handleRequestSort}
-              rowCount={rows.length}
-            />
-            <TableBody>
-              {visibleRows.map((row, index) => {
-                const labelId = `enhanced-table-checkbox-${index}`
-
-                return (
-                  <TableRow
-                    hover
-                    tabIndex={-1}
-                    key={row.name}
-                    sx={{ cursor: 'pointer' }}
-                  >
-                    <TableCell
-                      component="th"
-                      id={labelId}
-                      scope="row"
-                      padding="none"
-                      align="center"
-                    >
-                      {row.id}
-                    </TableCell>
-                    <TableCell align="left">{row.name}</TableCell>
-                    <TableCell align="left">{row.description}</TableCell>
-                    <TableCell align="left">{row.category}</TableCell>
-                    <TableCell align="left">
-                      {row.price.toLocaleString('pt-br', {
-                        style: 'currency',
-                        currency: 'BRL',
-                      })}
-                    </TableCell>
-                  </TableRow>
-                )
-              })}
-              {emptyRows > 0 && (
-                <TableRow
-                  style={{
-                    height: (dense ? 33 : 53) * emptyRows,
-                  }}
-                >
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-          labelRowsPerPage="Linhas por página:"
-        />
-        <Grid container justifyContent="space-between">
-          <Grid item sx={{ m: 2 }}>
-            <FormControlLabel
-              control={<Switch checked={dense} onChange={handleChangeDense} />}
-              label="Desativar espaçamento"
-            />
-          </Grid>
-          <Grid item sx={{ m: 2 }}>
-            <Button
-              variant="contained"
-              color="success"
-              sx={{ mr: 2 }}
-              onClick={() => router.push('/addproducts')}
+      <Grid container xl={10} lg={8} md={8} sm={8} xs={10}>
+        <Paper sx={{ width: '100%', mb: 2 }}>
+          <TableContainer>
+            <Table
+              sx={{ minWidth: 750 }}
+              aria-labelledby="tableTitle"
+              size={dense ? 'small' : 'medium'}
             >
-              Cadastrar novo produto
-            </Button>
+              <EnhancedTableHead
+                order={order}
+                orderBy={orderBy}
+                onRequestSort={handleRequestSort}
+                rowCount={rows.length}
+              />
+              <TableBody>
+                {visibleRows.map((row, index) => {
+                  const labelId = `enhanced-table-checkbox-${index}`
+
+                  return (
+                    <TableRow
+                      hover
+                      tabIndex={-1}
+                      key={row.name}
+                      sx={{ cursor: 'pointer' }}
+                    >
+                      <TableCell
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                        align="center"
+                      >
+                        {row.id}
+                      </TableCell>
+                      <TableCell align="left">{row.name}</TableCell>
+                      <TableCell align="left">{row.description}</TableCell>
+                      <TableCell align="left">{row.category}</TableCell>
+                      <TableCell align="left">
+                        {row.price.toLocaleString('pt-br', {
+                          style: 'currency',
+                          currency: 'BRL',
+                        })}
+                      </TableCell>
+                    </TableRow>
+                  )
+                })}
+                {emptyRows > 0 && (
+                  <TableRow
+                    style={{
+                      height: (dense ? 33 : 53) * emptyRows,
+                    }}
+                  >
+                    <TableCell colSpan={6} />
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            labelRowsPerPage="Linhas por página:"
+          />
+          <Grid container justifyContent="space-between">
+            <Grid item sx={{ m: 2 }}>
+              <FormControlLabel
+                control={
+                  <Switch checked={dense} onChange={handleChangeDense} />
+                }
+                label="Desativar espaçamento"
+              />
+            </Grid>
+            <Grid item sx={{ m: 2 }}>
+              <Button
+                variant="contained"
+                color="success"
+                sx={{ mr: 2 }}
+                onClick={() => router.push('/addproducts')}
+              >
+                Cadastrar novo produto
+              </Button>
+            </Grid>
           </Grid>
-        </Grid>
-      </Paper>
+        </Paper>
+      </Grid>
     </Box>
   )
 }
