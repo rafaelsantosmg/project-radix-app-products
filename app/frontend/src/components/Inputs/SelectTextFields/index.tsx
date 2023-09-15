adf5932c0e88bc992ad901faee7db640a9eae0ea
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2023-09-15 18:43:37 -0300

    styles(select)
    alteração da vaiação do select, e adcionado botão de limpar seleção

--- app/frontend/src/components/Inputs/SelectTextFields/index.tsx
+++ app/frontend/src/components/Inputs/SelectTextFields/index.tsx
@@ -1,17 +1,19 @@
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
 import MenuItem from '@mui/material/MenuItem'
+import RestartAltIcon from '@mui/icons-material/RestartAlt'
+import theme from '@/theme'
 
 export default function SelectTextFields({ ...props }) {
   const { label, data = [] } = props
   return (
-    <Box sx={{ width: '100%' }}>
+    <Box sx={{ width: '100%', position: 'relative' }}>
       <TextField
         id="standard-select-currency"
         select
         label={label}
         defaultValue={data.length ? data[0] : ''}
-        variant="filled"
+        variant="outlined"
         fullWidth
         {...props}
       >
@@ -21,6 +23,20 @@ export default function SelectTextFields({ ...props }) {
           </MenuItem>
         ))}
       </TextField>
+      {props.value !== '' && (
+        <RestartAltIcon
+          sx={{
+            position: 'absolute',
+            right: 40,
+            top: '50%',
+            transform: 'translateY(-50%)',
+            cursor: 'pointer',
+            fontSize: '1.2rem',
+            color: theme.gray,
+          }}
+          onClick={() => props.onChange({ target: { value: '' } })}
+        />
+      )}
     </Box>
   )
 }
