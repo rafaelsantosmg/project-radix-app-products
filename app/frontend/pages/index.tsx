5bb0e73a88f8ad08adab1370646374e85f779907
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2023-09-14 22:05:09 -0300

    feat(index)
    adcionado o Component searchbar a aplicação

--- app/frontend/pages/index.tsx
+++ app/frontend/pages/index.tsx
@@ -2,12 +2,14 @@ import SortTable from '../src/components/Table'
 import { Box } from '@mui/material'
 import Header from '../src/components/Header'
 import { styleHome } from '../styles/style-home'
+import SearchBar from '../src/components/SearchBar'
 
 export default function Home() {
   return (
     <Box sx={styleHome.main}>
       <Box sx={styleHome.mainWrapper}>
         <Header />
+        <SearchBar />
         <SortTable />
       </Box>
     </Box>
