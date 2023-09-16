c6cc77891014f9bf558e27c37afeb235c4aff633
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2023-09-16 17:15:11 -0300

    test(mock)
    adcionado o arquivo de mock dos produtos

---
+++ app/backend/tests/mocks/index.ts
@@ -0,0 +1,30 @@
+export const allProducts = [
+  {
+    id: '1',
+    name: 'Smartphone XYZ',
+    description: 'Um smartphone de última geração',
+    category: 'Eletrônicos',
+    price: 799.99,
+  },
+  {
+    id: '2',
+    name: 'Câmera Digital HD',
+    description: 'Uma câmera digital de alta definição',
+    category: 'Fotografia',
+    price: 349.99,
+  },
+  {
+    id: '3',
+    name: 'Tênis de Corrida',
+    description: 'Tênis esportivo para corridas',
+    category: 'Esportes',
+    price: 89.99,
+  },
+  {
+    id: '4',
+    name: 'Livro de Ficção Científica',
+    description: 'Um emocionante romance de ficção científica',
+    category: 'Livros',
+    price: 19.99,
+  },
+]
